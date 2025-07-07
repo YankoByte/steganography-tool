d10cca698bbe0b71b7d01dc11a25eb81cb08f49c
YankoByte <yankobrrrau@gmail.com>
2025-07-07 21:55:57 +1000

    Added basic functionality for encoding information using LSB manipulation using a fingerprinting system

--- Helpers.py
+++ Helpers.py
@@ -1,9 +1,29 @@
 # COMP6841 - Steganography Project Helpers File
 
 import os
+import hashlib
+
+HASHSIZE = 64
 
 def clearTerminal():
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
+
+def stringToBinary(string):
+    return ''.join(format(ord(char), '08b') for char in string)
+
+def hashGenerator(string):
+    data = string.encode()
+    return hashlib.sha256(data).hexdigest()
+
+def dataEncoder(info, hash):
+    firstHalf = slice(HASHSIZE//2)
+    lastHalf = slice(HASHSIZE//2, HASHSIZE)
+
+    firstFingerprint = hash[firstHalf]
+    lastFingerprint = hash[lastHalf]
+
+    data = firstFingerprint + info + lastFingerprint
+    return stringToBinary(data)
