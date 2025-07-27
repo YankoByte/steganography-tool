ca803a4a50a56d2e1ebb0dd361483f1c8c6eeb0f
YankoByte <yankobrrrau@gmail.com>
2025-07-28 01:21:16 +1000

    Fixed error regarding import modules

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -15,7 +15,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from PIL import Image
 
-from displayScripts import *
+from .displayScripts import *
 
 HASHSIZE = 64
 
